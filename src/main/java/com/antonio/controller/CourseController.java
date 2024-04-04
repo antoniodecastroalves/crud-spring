c67757853f7f1e80631d8d52a49a571ec33fba4b
Antonio Alves <antoniodecastroalves@gmail.com>
2024-04-03 22:44:34 -0300

    git commit -m ":sparkles: feat: Course model - part 2"

--- src/main/java/com/antonio/controller/CourseController.java
+++ src/main/java/com/antonio/controller/CourseController.java
@@ -1,6 +1,7 @@
 package com.antonio.controller;
 import java.util.List;
 
+import com.antonio.model.Course;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class CourseController {
 
   @GetMapping
-  public List<Object> list() {
+  public List<Course> list() {
+
     return null;
   }
   
